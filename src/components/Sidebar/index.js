b4ad24b49ca076e5412b2e800b38c5d232cc5a64
kalmtalyst <kalmtalyst@gmail.com>
2022-05-13 17:13:05 +0900

    fix: modify style on layout and sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,25 +1,59 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope,faHome,faMusic } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faInstagram,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <nav>
-        <NavLink exact="true" 
-        className='home-link' 
-        activeclassname="active" to='/'>
-          <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+        <NavLink
+          exact="true"
+          className="home-link"
+          activeclassname="active"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" 
-        className='music-link' activeclassname="active" to='/music'>
-     <FontAwesomeIcon icon={faMusic } />
+        <NavLink
+          exact="true"
+          className="about-link"
+          activeclassname="active"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" className='contact-link' to='/contact'>
-          <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
